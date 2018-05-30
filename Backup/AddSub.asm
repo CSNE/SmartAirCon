aa76efb3397739f354b0f958c4022178cbaa5170
lky7674 <3822c@naver.com>
2018-05-30 18:24:38 +0900

    역할
    뀪뀪

--- Backup/AddSub.asm
+++ Backup/AddSub.asm
@@ -18,6 +18,7 @@ ReadInput  PROC
 	;ax -> 아스키?
 	;bx -> 숫자*10
 	;cx -> 숫자*10 => 옵션
+	;찬솔
 	RET
 ReadInput ENDP
 
@@ -25,16 +26,19 @@ Manual PROC
 	;x1,x2,y1,y2,a,b를 사용해서 anew, bnew를 구하고 k를 감소
 	;a,b -> anew, bnew
 	;Manual : 현재 온도가 같은 상태는 두번 저장하지 않는다(?)
+	;뀪뀪이
 	RET
 Manual ENDP
 
 Automatic PROC
 	;x -> ax 저장 -> 변환 후 -> y에 저장
+	;누나
 	RET
 Automatic ENDP
 
 Output PROC
 	;ax로 숫자 * 10을 받고 출력함
+	;찬솔
 	RET
 Output ENDP
 
@@ -50,6 +54,7 @@ main PROC
 	Automatic PROTO
 	Output PROTO
 	JukJulHan PROTO
+	;누나 잘 부탁해!
 	exit
 main ENDP
 END main
