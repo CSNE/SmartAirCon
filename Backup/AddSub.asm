9492cd132480298b3367104518c612eea4645aad
lky7674 <3822c@naver.com>
2018-05-30 17:32:35 +0900

    프로젝트32
    한글 사랑

---
+++ Backup/AddSub.asm
@@ -0,0 +1,15 @@
+TITLE Add and Subtract (AddSub.asm)
+
+INCLUDE Irvine32.inc
+
+.data
+myBytes   BYTE 10h,20h,30h,40h
+
+.code
+main PROC
+	mov al,BYTE PTR myBytes
+	mov bl,BYTE PTR myBytes+1
+	mov cl,BYTE PTR myBytes+2
+	exit
+main ENDP
+END main
