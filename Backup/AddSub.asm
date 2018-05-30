4d543ac8046be901c84d9a8bf5eb27c895f58979
lky7674 <3822c@naver.com>
2018-05-30 18:14:28 +0900

    정리
    요약문

--- Backup/AddSub.asm
+++ Backup/AddSub.asm
@@ -1,13 +1,55 @@
 INCLUDE Irvine32.inc
 
 .data
-myBytes   BYTE 10h,20h,30h,40h
+a SWORD 10
+b SWORD 50
+a1 SWORD ?
+b1 SWORD ?
+anew SWORD ?
+bnew SWORD ?
+k SWORD 10
+x1 SWORD -50
+y1 SWORD -50
+x2 SWORD 1000
+y2 SWORD 1000
 
 .code
 main PROC
-	mov al,BYTE PTR myBytes
-	mov bl,BYTE PTR myBytes+1
-	mov cl,BYTE PTR myBytes+2
+	PROTO ReadInput
+	PROTO Manual
+	PROTO Automatic
+	PROTO Output
+	PROTO JukJulHan
 	exit
 main ENDP
 END main
+
+ReadInput  PROC
+	;ax -> 아스키?
+	;bx -> 숫자*10
+	;cx -> 숫자*10 => 옵션
+	RET
+ReadInput ENDP
+
+Manual PROC
+	;x1,x2,y1,y2,a,b를 사용해서 anew, bnew를 구하고 k를 감소
+	;a,b -> anew, bnew
+	;Manual : 현재 온도가 같은 상태는 두번 저장하지 않는다(?)
+	RET
+Manual ENDP
+
+Automatic PROC
+	;x -> ax 저장 -> 변환 후 -> y에 저장
+	RET
+Automatic ENDP
+
+Output PROC
+	;ax로 숫자 * 10을 받고 출력함
+	RET
+Output ENDP
+
+JukJulHan PROC
+	;적절하면 ax=1
+	MOV ax,1
+	RET
+JukJulHan ENDP
