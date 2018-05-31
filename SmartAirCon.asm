73498698d588d6170a801b3d9dde389a0c0aad96
CSNE <chansol98@gmail.com>
2018-06-01 00:30:49 +0900

    ReadInput 구현함.

--- SmartAirCon.asm
+++ SmartAirCon.asm
@@ -30,6 +30,19 @@ debugMsg1 BYTE "Opening File...", 0dh, 0ah, 0
 debugMsg2 BYTE "File opened successfully.", 0dh, 0ah, 0
 debugMsg3 BYTE "File handle: ", 0
 debugMsg4 BYTE "Read success. Bytes read: ", 0
+debugMsg5 BYTE "ReadInput Finish", 0
+debugMsg6 BYTE "Readinput Noline", 0
+debugMsg7 BYTE "Readinput unexpected char: ", 0
+debugMsg8 BYTE "ReadInput Loop1", 0
+debugMsg9 BYTE "ReadInput Loop2", 0
+debugMsg10 BYTE "ReadInput Loop3", 0
+debugMsg11 BYTE "char:", 0
+
+dwTempVar1 DWORD ?
+dwTempVar2 DWORD ?
+dwTempVar3 DWORD ?
+dwTempVar4 DWORD ?
+dwTempVar5 DWORD ?
 
 ReadInput PROTO
 Manual PROTO
@@ -88,10 +101,30 @@ mainloop:
 	
 	;모든 줄 읽으려면 루프 돌아야되는데 이거 메인에서 하나..? 메인 은근 할거 많은데?!!?!? 나중에 함수들 다짜고 짜야겄구만!
 	
-	INVOKE ReadInput				;ReadInput을 어떻게 구현할지 몰라서 파일 열어만 뒀엉
 
+	readLoop:
+		INVOKE ReadInput				;ReadInput을 어떻게 구현할지 몰라서 파일 열어만 뒀엉
+
+		ADD eax,0
+		JZ readLoopEnd
+
+
+		CALL WriteChar
+		CALL Crlf
+		MOV eax,ebx
+		CALL WriteInt
+		CALL Crlf
+		MOV eax,ecx
+		CALL WriteInt
+		CALL Crlf
+		CALL Crlf
+
+		JMP readLoop
+
+	readLoopEnd:
 
 	jmp close_file
+
 if_manual:
 
 
@@ -126,12 +159,203 @@ quit:
 main ENDP
 
 ReadInput  PROC
-	;ax -> 아스키? ('a'=97 | 'm'=109)
-	;bx -> 숫자*10
-	;cx -> 숫자*10 => 옵션
+	;eax -> 아스키? ('a'=97 | 'm'=109)
+	;ebx -> 숫자*10
+	;ecx -> 숫자*10 => 옵션
 	;더 읽을 게 없다면 ax=0
 	;찬솔
-	RET
+
+
+	; *** INTERNAL VARIABLES ***
+	;ECX: currently reading char address
+	;BL: current character value
+	;EAX,EDX: temporary
+
+	;dwTempVar1: 1st part result
+	;dwTempVar2: 2nd part result
+	;dwTempVar3: 3rd part result
+
+	MOV dwTempVar1,0
+	MOV dwTempVar2,0
+	MOV dwTempVar3,0
+
+
+	MOV ecx, OFFSET buffer
+	ADD ecx, readProgress ;ECX=first char address
+
+	firstPartLoop:
+		;get current char
+		MOV bl, BYTE PTR [ecx]
+		INC ecx ;next character address
+
+		;디버깅용 메시지
+		;mov edx, OFFSET debugMsg8
+		;call WriteString
+		;call Crlf
+		;mov edx, OFFSET debugMsg11
+		;call WriteString
+		;MOV al,bl
+		;call WriteChar
+		;call Crlf
+
+		CMP bl, 13 ;compare BL to \r
+		JE firstPartLoop ;ignore \r
+
+		CMP bl, 10 ;compare BL to \n
+		JE unexpected ;\n shouldn't appear in 1st part
+
+		CMP bl, 0 ;compare BL to \0
+		JE noline ;\0 in 1st part means there's no line here.
+
+		CMP bl, 32 ;compare BL to space
+		JE secondPartLoop ;if space, go to next part
+
+
+		;dwTempVar1 should be zero here
+		;if not, it means the first part is >=2 chars long.
+		CMP dwTempVar1, 0 
+		JNE unexpected
+
+		;store in dwTempVar1
+		MOVZX eax, bl
+		MOV dwTempVar1,eax
+
+		;loop forever
+		JMP firstPartLoop
+
+
+
+	secondPartLoop:
+
+		;get current char
+		MOV bl, BYTE PTR [ecx]
+		INC ecx ;next character address
+
+		;디버깅용 메시지
+		;mov edx, OFFSET debugMsg9
+		;call WriteString
+		;call Crlf
+		;mov edx, OFFSET debugMsg11
+		;call WriteString
+		;MOV al,bl
+		;call WriteChar
+		;call Crlf
+
+		CMP bl, 13 ;compare BL to \r
+		JE secondPartLoop ;continue if \r
+
+		CMP bl, 10 ;compare BL to \n
+		JE finished ;\n is the newline char - EOL, end!
+
+		CMP bl, 0 ;compare BL to \0
+		JE finished ;0 may appear is there is no newline at EOF.
+
+		CMP bl, 32 ;compare BL to space
+		JE thirdPartLoop ;goto next part
+
+		CMP bl, 46 ;compare BL to .
+		JE secondPartLoop ;ignore
+
+		
+		;dwTempVar2*=10
+		MOV eax, dwTempVar2
+		CDQ
+		MOV edx,10
+		MUL edx
+		MOV dwTempVar2, eax
+
+		SUB bl, 48 ; ascii('0')=48. This converts bl to int.
+		MOVZX eax, bl
+		ADD dwTempVar2, eax
+
+		
+		JMP secondPartLoop
+
+	thirdPartLoop:
+		
+		;get current char
+		MOV bl, BYTE PTR [ecx]
+		INC ecx ;next character address
+
+		;디버깅용 메시지
+		;mov edx, OFFSET debugMsg10
+		;call WriteString
+		;call Crlf
+		;mov edx, OFFSET debugMsg11
+		;call WriteString
+		;MOV al,bl
+		;call WriteChar
+		;call Crlf
+
+		CMP bl, 13 ;compare BL to \r
+		JE thirdPartLoop ;continue if \r
+
+		CMP bl, 10 ;compare BL to \n
+		JE finished ;\n is the newline char
+
+		CMP bl, 0 ;compare BL to \0
+		JE finished ;0 may appear is there is no newline at EOF.
+
+		CMP bl, 32 ;compare BL to space
+		JE unexpected ;Space must not appear in 3rd part.
+
+		;dwTempVar3*=10
+		MOV eax, dwTempVar3
+		CDQ
+		MOV edx,10
+		MUL edx
+		MOV dwTempVar3, eax
+
+		SUB bl, 48 ; ascii('0')=48. This converts bl to int.
+		MOVZX eax, bl
+		ADD dwTempVar3, eax
+
+		JMP thirdPartLoop
+		
+	unexpected: ;예상 밖의 문자가 나왔을시 점프됨
+		;디버깅용 메시지
+		mov edx, OFFSET debugMsg7
+		call WriteString
+		call Crlf
+		mov edx, OFFSET debugMsg11
+		call WriteString
+		MOV al,bl
+		call WriteChar
+		call Crlf
+
+		JMP finished
+	noline: ;더 이상 줄이 없을때 점프됨
+		;디버깅용 메시지
+		mov edx, OFFSET debugMsg6
+		call WriteString
+		call Crlf
+
+		JMP finished
+	finished: ;정리 및 리턴
+		;디버깅용 메시지
+		;mov edx, OFFSET debugMsg5
+		;call WriteString
+		;call Crlf
+
+		SUB ecx, OFFSET buffer
+		MOV readProgress,ecx
+
+		;디버깅용 메시지
+		;MOV eax,ecx
+		;CALL WriteInt
+		;Call Crlf
+
+		;dwTempVar3*=10
+		MOV eax, dwTempVar3
+		CDQ
+		MOV edx,10
+		MUL edx
+		MOV dwTempVar3, eax
+
+		MOV eax, dwTempVar1
+		MOV ebx, dwTempVar2
+		MOV ecx, dwTempVar3
+		RET
 ReadInput ENDP
 
 Manual PROC
